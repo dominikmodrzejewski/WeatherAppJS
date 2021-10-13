2312e87583ce2287adb9ee1980ad5f5a418f79c2
Dominik Modrzejewski <dominikmodrzejewski@pw.pl>
2021-10-13 11:35:58 +0200

    finished app

--- script.js
+++ script.js
@@ -7,7 +7,55 @@ const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
 
-const API_LINK = 'api.openweathermap.org/data/2.5/weather?q='
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
 
-const API_KEY = '&appid=37f94956ea562a1a69007481d5f8689a'
+const API_KEY = '&appid=13168de3e5b9fd6fa44f11b359fc2e76'
 const API_UNITS = '&units=metric'
+
+const getWeather = () => {
+	const city = input.value 
+	const URL = API_LINK + city + API_KEY + API_UNITS
+
+	axios
+		.get(URL)
+		.then(res => {
+			const status = Object.assign({}, ...res.data.weather)
+
+			warning.textContent = ''
+			input.value = ''
+
+			if (status.id >= 200 && status.id < 300) {
+				photo.setAttribute('src', './img/thunderstorm.png')
+			} else if (status.id >= 300 && status.id < 400) {
+				photo.setAttribute('src', './img/drizzle.png')
+			} else if (status.id >= 500 && status.id < 600) {
+				photo.setAttribute('src', './img/rain.png')
+			} else if (status.id >= 600 && status.id < 700) {
+				photo.setAttribute('src', './img/ice.png')
+			} else if (status.id >= 700 && status.id < 800) {
+				photo.setAttribute('src', './img/fog.png')
+			} else if (status.id === 800) {
+				photo.setAttribute('src', './img/sun.png')
+			} else if (status.id > 800 && status.id < 900) {
+				photo.setAttribute('src', './img/cloud.png')
+			} else {
+				photo.setAttribute('src', './img/unknown.png')
+			}
+
+			cityName.textContent = res.data.name
+			temperature.textContent = Math.floor(res.data.main.temp) + '℃'
+			humidity.textContent = res.data.main.humidity + '%'
+			weather.textContent = status.main
+		})
+		.catch(() => (warning.textContent = 'Wpisz poprawną nazwę miasta!'))
+}
+
+const enterKeyCheck = e => {
+	if (e.key == 'Enter') {
+		getWeather()
+	}
+}
+
+getWeather()
+input.addEventListener('keyup',enterKeyCheck)
+button.addEventListener('click', getWeather)
